efca58c9887870f7c38e4df7b7e41a981b7b2b39
Angelo <angelo.francisco0002@gmail.com>
2025-07-28 11:00:10 -0300

    ADD: melhorando header.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,17 @@
 <header>
-    <nav class="navbar bg-body-tertiary" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand">Navbar</a>
+            <a href="{{ route('cars.index') }}" class="navbar-brand">Home</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('cars.index') }}">Car</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('cars.index') }}">Motorcycle</a>
+                    </li>
+                </ul>
+            </div>
             @if(Route::current()->uri() !== '{id}')
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
